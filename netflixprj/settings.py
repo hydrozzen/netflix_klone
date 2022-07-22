7c0bfc2d8afded2a7eaa79f4e58c667ee149e8b9
hydrozzen <105576603+hydrozzen@users.noreply.github.com>
2022-07-22 19:19:24 +0530

    Update settings.py
    Signed-off-by: hydrozzen <105576603+hydrozzen@users.noreply.github.com>

--- netflixprj/settings.py
+++ netflixprj/settings.py
@@ -142,8 +142,8 @@ SITE_ID = 1
 AUTH_USER_MODEL = 'netflixapp.CustomUser'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
