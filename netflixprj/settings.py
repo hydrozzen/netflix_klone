c7e39d72392897aa9a263719ea0d47b5d1d88a49
hydrozzen <105576603+hydrozzen@users.noreply.github.com>
2022-07-22 19:12:27 +0530

    Update settings.py
    Signed-off-by: hydrozzen <105576603+hydrozzen@users.noreply.github.com>

--- netflixprj/settings.py
+++ netflixprj/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
